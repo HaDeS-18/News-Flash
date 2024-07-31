e0eb9901802e0fe761525fc36159d46e511af249
Harsh Agrawal <harsh.agrawal116611@gmail.com>
2024-07-31 20:11:00 +0530

    Initial Commit    What's New:       - Base project - News App v1       - Implements Clean Architecture

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,3 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPI = 'c92bc5c5a6fa44a8848c2524406662e4';
+const String sourceQuery = 'techcrunch';
---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,16 @@
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:retrofit/retrofit.dart';
+import '../../../../../core/constants/constants.dart';
+import 'package:dio/dio.dart';
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl:newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+  
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("sources") String ? sources,
+    @Query("apiKey") String ? apiKey,
+  });
+}
